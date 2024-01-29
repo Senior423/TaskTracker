772fe43ab046a670e70c6fdd2bc4c29a7e348c59
Артемий1~ <senior423it@gmail.com>
2024-01-29 23:11:04 +0200

    feat(render.js): module

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 "use strict"
 
-let todos = JSON.parse(localStorage.getItem('todos')) || []
+import { renderTask } from "./render.js"
+
+export let todos = JSON.parse(localStorage.getItem('todos')) || []
 
 const root = document.getElementById('root')
 const control = document.createElement('div')
@@ -74,92 +76,8 @@ function createTask(event) {
 
 buttonSubmit.addEventListener('click', (event) => createTask(event))
 
-const taskList = document.createElement('div')
+export const taskList = document.createElement('div')
 taskList.classList.add('task__list')
 root.append(taskList)
-
-function renderTask() {
-    taskList.innerHTML = ''
-    if (todos.length > 0) {
-        todos.forEach(({id, date, text, isChecked}) => createTaskCard({id, date, text, isChecked}))
-    }
-}
-
-function createTaskCard({id, date, text, isChecked}) {
-    const taskCard = document.createElement('div')
-    taskCard.classList.add('task__card')
-    taskCard.id = id
-    taskList.append(taskCard)
-    createCheckbox({id, date, text, isChecked}, taskCard)
-}
-
-function createCheckbox({id, date, text, isChecked}, taskCard) {
-    const checkbox = document.createElement('input')
-    checkbox.classList.add('task__checkbox')
-    checkbox.type = 'checkbox'
-    checkbox.defaultChecked = isChecked
-    taskCard.append(checkbox)
-    checkbox.addEventListener('click', () => examChecked(checkbox, id))
-    createText({id, date, text}, taskCard)
-}
-
-function examChecked(checkbox, id) {
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id === id) {
-            todos[i].isChecked = checkbox.checked
-            localStorage.setItem('todos', JSON.stringify(todos))
-            break
-        }
-    }
-}
-
-function createText({id, date, text}, taskCard) {
-    const description = document.createElement('input')
-    description.classList = 'task__description'
-    description.type = 'input'
-    description.value = text
-    taskCard.append(description)
-    createDate({id, date}, taskCard)
-    description.addEventListener('input', () => selText(id, description))
-}
-
-function selText(id, description) {
-    for (let i = 0; i < todos.length; i++) {
-        if (todos[i].id === id) {
-            todos[i].text = description.value
-            localStorage.setItem('todos', JSON.stringify(todos))
-            break
-        }
-    }
-}
-
-function createDate({id, date}, taskCard) {
-    const taskDate = document.createElement('p')
-    taskDate.classList.add('task__date')
-    taskDate.textContent = date
-    taskCard.append(taskDate)
-    createButtonDelete(id, taskCard)
-}
-
-function createButtonDelete(id, taskCard) {
-    const button = document.createElement('button')
-    button.className = 'fa-solid fa-trash-can task__button-delete'
-    taskCard.append(button)
-    button.addEventListener('click', () => taskDelete(id))
-}
-
-function taskDelete(id) {
-    const confirmation = confirm('Вы уверены, что хотите удалить эту задачу?')
-    if (confirmation) {
-        for (let i = 0; i < todos.length; i++) {
-            if (todos[i].id === id) {
-                todos.splice(i, 1)
-                renderTask()
-                localStorage.setItem('todos', JSON.stringify(todos))
-                break
-            }
-        }
-    }
-}
 
 renderTask()
